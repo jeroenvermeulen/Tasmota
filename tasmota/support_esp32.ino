72315840446cadb704c99835b05f32ee5bfa67d0
gemu2015 <gmutz2010@googlemail.com>
2020-12-26 09:38:08 +0100

    Update support_esp32.ino

--- tasmota/support_esp32.ino
+++ tasmota/support_esp32.ino
@@ -374,8 +374,8 @@ uint32_t ESP_getSketchSize(void) {
 }
 
 uint32_t ESP_getFreeHeap(void) {
-//  return ESP.getFreeHeap();
-  return ESP.getMaxAllocHeap();
+  return ESP.getFreeHeap();
+  //return ESP.getMaxAllocHeap();
 }
 
 uint32_t ESP_getMaxAllocHeap(void) {
